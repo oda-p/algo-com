8f3cadb0b9644a09c67a30fc0f5f2d69d8044865
duhee <duhee.lee@influential.co.kr>
2023-11-27 18:36:27 +0900

    SameTree 문제풀이

---
+++ leecode/easy/SameTree.java
@@ -0,0 +1,17 @@
+package easy;
+class SameTree {
+    class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if(p != null && q != null && p.val != q.val) {
+                return false;
+            }
+            if(p == null && q == null){
+                return true;
+            }
+            else if (p == null || q == null) {
+                return false;
+            }
+            return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
+        }
+    }이
+}
