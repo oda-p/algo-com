e682c9359bf6ccdb33caad0cb051d9a544577aad
duhee.lee <duhee74@naver.com>
2023-12-01 21:07:54 +0900

    임시단순화

--- leetcode/easy/RemoveElement.java
+++ leetcode/easy/RemoveElement.java
@@ -1,23 +1,21 @@
 package easy;
 
-class RemoveElement{
-    class Solution {
-        public int removeElement(int[] nums, int val) {
-            if(nums.length == 0){
-                return 0;
-            }
-            int[] newArray = new int[nums.length];
-            int j = 0;
-            for(int i=0; i < nums.length; i++){
-                if(nums[i] != val){
-                    newArray[j] = nums[i];
-                    j += 1;
-                }
-            }
-            for(int i=0; i < j; i++){
-                nums[i] = newArray[i];
-            }
-            return j;
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if(nums.length == 0){
+            return 0;
         }
-    };
-}
+        int[] newArray = new int[nums.length];
+        int j = 0;
+        for(int i=0; i < nums.length; i++){
+            if(nums[i] != val){
+                newArray[j] = nums[i];
+                j += 1;
+            }
+        }
+        for(int i=0; i < j; i++){
+            nums[i] = newArray[i];
+        }
+        return j;
+    }
+};
--- leetcode/easy/SameTree.java
+++ leetcode/easy/SameTree.java
@@ -1,17 +1,16 @@
 package easy;
-class SameTree {
-    class Solution {
-        public boolean isSameTree(TreeNode p, TreeNode q) {
-            if(p != null && q != null && p.val != q.val) {
-                return false;
-            }
-            if(p == null && q == null){
-                return true;
-            }
-            else if (p == null || q == null) {
-                return false;
-            }
-            return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
+import TreeNode;
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p != null && q != null && p.val != q.val) {
+            return false;
         }
+        if(p == null && q == null){
+            return true;
+        }
+        else if (p == null || q == null) {
+            return false;
+        }
+        return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
     }
 }
